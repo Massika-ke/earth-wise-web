8544139063781076df533d47983825785ced2afb
Massika-Ke <wamwoka99@gmail.com>
2025-02-08 00:12:15 +0300

    Feature; search-bar

--- components/Header.tsx
+++ components/Header.tsx
@@ -222,7 +222,9 @@ export default function Header({ onMenuClick, totalEarnings}: HeaderProps){
                 {!isMobile && (
                     <div className="flex-1 max-w-xl mx-4">
                         <div className="relative">
-                            <input type="text" placeholder="search..." className="" />
+                            <input type="text" placeholder="search..." 
+                            className="w-full px-4 py-2 border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-green-500" />
+                            <Search className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
                         </div>
                     </div>
                 )}
